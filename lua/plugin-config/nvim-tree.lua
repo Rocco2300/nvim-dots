565d6aa2f29f7b0c75e85b60a33efda048a7ba3c
Grigore Andrei <grigoreandrey23@gmail.com>
2023-03-31 14:38:56 +0300

    add keybinds for nvim-tree

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,6 +1,22 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.tree_open = false
 
 vim.opt.termguicolors = true
 
 require('nvim-tree').setup()
+
+vim.keymap.set('n', '<leader>t', 
+function()
+    vim.cmd('NvimTreeToggle')
+    vim.g.tree_open = not vim.g.tree_open
+end
+)
+
+vim.keymap.set('n', '<leader>f', 
+function()
+    if vim.g.tree_open then
+        vim.cmd('NvimTreeFocus')
+    end
+end
+)
