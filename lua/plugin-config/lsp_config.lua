c8a7bead56f6602f8cecbbd94b358349f4b6494f
Grigore Andrei <grigoreandrey23@gmail.com>
2023-04-02 14:16:54 +0300

    add global var

--- lua/plugin-config/lsp_config.lua
+++ lua/plugin-config/lsp_config.lua
@@ -14,7 +14,15 @@ local on_attach = function(_, _)
 end
 
 require('lspconfig').lua_ls.setup({
-    on_attach = on_attach
+    on_attach = on_attach,
+
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 require('lspconfig').clangd.setup({
